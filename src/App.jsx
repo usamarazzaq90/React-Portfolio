df9691b03a454b0cf82c550e722b87718948db92
usamarazzaq90 <usamarazzaq9014@gmail.com>
2024-09-28 11:52:48 +0500

    added updates

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home3 />} />
-          <Route path="home-v1" element={<Home />} />
-          <Route path="home-v4" element={<Home4 />} />
+          {/* <Route path="home-v1" element={<Home />} /> */}
+          {/* <Route path="home-v4" element={<Home4 />} /> */}
           <Route path="*" element={<Page404 />} />
           <Route path="blog/blog-details" element={<BlogDetails />} />
         </Route>
-        <Route path="/home-v2" element={<Layout2 />}>
+        {/* <Route path="/home-v2" element={<Layout2 />}>
           <Route index element={<Home2 />} />
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/Resume/SingleResume.jsx
+++ src/components/Resume/SingleResume.jsx
@@ -1,19 +1,21 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 const SingleResume = ({ element }) => {
-  const { title, duration, subTitle, text} = element;
+  const { title, duration, subTitle, text } = element;
   return (
     <div className="st-resume-timeline">
       <h3 className="st-resume-timeline-title">{title}</h3>
       <div className="st-resume-timeline-duration">{duration}</div>
       <h4 className="st-resume-timeline-subtitle">{subTitle}</h4>
-      <div className="st-resume-timeline-text"><p>{text}</p></div>
+      <div className="st-resume-timeline-text">
+        <p>{text}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 SingleResume.propTypes = {
   element: PropTypes.object,
-}
+};
 
 export default SingleResume;
